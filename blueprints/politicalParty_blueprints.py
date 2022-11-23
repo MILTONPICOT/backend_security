81db0806b103e104bc660ddb479b5c636fe890b2
Mzrbx <111446439+Mzrbx@users.noreply.github.com>
2022-11-23 18:39:33 -0500

    Update politicalParty_blueprints.py

--- blueprints/politicalParty_blueprints.py
+++ blueprints/politicalParty_blueprints.py
@@ -1,42 +1,41 @@
 from flask import Blueprint
 from flask import request
 
-from controller.politicalParty_controller import PoliticalPartyController
+from controllers.politicalpartyController import PoliticalPartyController
 
-politicalParty_blueprints = Blueprint('politicalParty_blueprints', __name__)
-politicalParty_controller = PoliticalPartyController()
+politicalparty_blueprints = Blueprint('politicalparty_blueprints', __name__)
+politicalparty_controller = PoliticalPartyController()
 
 
-@politicalParty_blueprints.route("/politicalParty/all", methods=['GET'])
-def get_all_political_party():
-    response = politicalParty_controller.index()
+@politicalparty_blueprints.route("/politicalparty/all", methods=['GET'])
+def get_all_politicalparty():
+    response = politicalparty_controller.index()
     return response, 200
 
 
-@politicalParty_blueprints.route("/politicalParty/<string:id_>", methods=['GET'])
-def get_political_party_by_id(id_):
-    response = politicalParty_controller.show(id_)
+@politicalparty_blueprints.route("/politicalparty/<string:id_>", methods=['GET'])
+def get_politicalparty_by_id(id_):
+    response = politicalparty_controller.show(id_)
     return response, 200
 
 
-@politicalParty_blueprints.route("/politicalParty/insert", methods=['POST'])
-def insert_political_party():
-    politicalParty = request.get_json()
-    response = politicalParty_controller.Create(politicalParty)
+@politicalparty_blueprints.route("/politicalparty/insert", methods=['POST'])
+def insert_politicalparty():
+    politicalparty = request.get_json()
+    response = politicalparty_controller.create(politicalparty)
     return response, 201
 
 
-@politicalParty_blueprints.route("/politicalParty/update/<string:id_>", methods=['PATCH'])
-def update_political_party(id_):
-    politicalParty = request.get_json()
-    response = politicalParty_controller.Update(id_, politicalParty)
+@politicalparty_blueprints.route("/politicalparty/update/<string:id_>", methods=['PATCH'])
+def update_politicalparty(id_):
+    politicalparty = request.get_json()
+    response = politicalparty_controller.update(id_, politicalparty)
     return response, 201
 
 
-@politicalParty_blueprints.route("/politicalParty/delete/<string:id_>", methods=['DELETE'])
-def delete_political_party(id_):
-    response = politicalParty_controller.Delete(id_)
+@politicalparty_blueprints.route("/politicalparty/delete/<string:id_>", methods=['DELETE'])
+def delete_politicalparty(id_):
+    response = politicalparty_controller.delete(id_)
     return response, 204
-
 
 
