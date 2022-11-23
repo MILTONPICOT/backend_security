77e83343c9f4ff61c3e88111d3710c57358d5118
Mzrbx <111446439+Mzrbx@users.noreply.github.com>
2022-11-23 18:36:25 -0500

    Update candidate_blueprints.py

--- blueprints/candidate_blueprints.py
+++ blueprints/candidate_blueprints.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask import request
 
-from controller.candidate_controller import CandidateController
+from controllers.candidateController import CandidateController
 
 candidate_blueprints = Blueprint('candidate_blueprints', __name__)
 candidate_controller = CandidateController()
@@ -15,29 +15,34 @@ def get_all_candidate():
 
 @candidate_blueprints.route("/candidate/<string:id_>", methods=['GET'])
 def get_candidate_by_id(id_):
-    response = candidate_controller.Show(id_)
+    response = candidate_controller.show(id_)
     return response, 200
 
 
 @candidate_blueprints.route("/candidate/insert", methods=['POST'])
 def insert_candidate():
     candidate = request.get_json()
-    response = candidate_controller.Create(candidate)
+    response = candidate_controller.create(candidate)
     return response, 201
 
 
 @candidate_blueprints.route("/candidate/update/<string:id_>", methods=['PATCH'])
 def update_candidate(id_):
     candidate = request.get_json()
-    response = candidate_controller.Update(id_, candidate)
+    response = candidate_controller.update(id_, candidate)
+    return response, 201
+
+
+@candidate_blueprints.route("/candidate/<string:candidate_id>/politicalparty/<string:politicalparty_id>", methods=['PUT'])
+def assign_politicalparty(candidate_id, politicalparty_id):
+    response = candidate_controller.department_assign(candidate_id, politicalparty_id)
     return response, 201
 
 
 @candidate_blueprints.route("/candidate/delete/<string:id_>", methods=['DELETE'])
 def delete_candidate(id_):
-    response = candidate_controller.Delete(id_)
+    response = candidate_controller.delete(id_)
     return response, 204
-
 
 
 
