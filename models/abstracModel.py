a710782bf0f11fc08f81277c67ceffe2269d5af8
Mzrbx <111446439+Mzrbx@users.noreply.github.com>
2022-11-23 18:57:52 -0500

    Delete abstracModel.py

--- models/abstracModel.py
+++
@@ -1,8 +0,0 @@
-from abc import ABCMeta
-
-
-class AbstractModel(metaclass=ABCMeta):
-
-    def __init__(self, data: dict):
-        for key, value in data.items():  # Convertir clave valor en un listado de tuplas
-            setattr(self, key, value)
