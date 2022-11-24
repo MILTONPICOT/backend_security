01a11ec903d70e3d9fa49b6114abe25b7fd7d8be
Mzrbx <111446439+Mzrbx@users.noreply.github.com>
2022-11-23 19:26:49 -0500

    Add files via upload

---
+++ test_db_connection.py
@@ -0,0 +1,13 @@
+import pymongo
+import certifi
+
+ca = certifi.where()
+client = pymongo.MongoClient
+
+client = pymongo.MongoClient(
+    "mongodb+srv://Mzrbx7:Jod345@mticc4votaciones.ymr6g60.mongodb.net/votaciones_db?retryWrites=true&w=majority",
+    tlsCAFile =ca
+)
+db = client.test
+print(db)
+
