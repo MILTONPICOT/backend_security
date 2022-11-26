4b4177a68d15a3d930424a641624b8c5805743e1
Mzrbx <111446439+Mzrbx@users.noreply.github.com>
2022-11-25 19:28:10 -0500

    Update reports_repository.py

--- repositories/reports_repository.py
+++ repositories/reports_repository.py
@@ -146,4 +146,55 @@ class ReportRepository(InterfaceRepository[Result]):
         }
         pipline = [query_lookup, query_unwind, query_group, query_add_fields]
         return self.query_aggregation(pipline)
+    
+        def get_result_by_politicalparty(self):
+        query_lookup1 = {
+            "$lookup": {
+                "from": "candidate",
+                "localField": "candidate.$id",
+                "foreignField": "_id",
+                "as": "candidate_info"
+            }
+        }
+        query_unwind1 = {
+            "$unwind": "$candidate_info"
+        }
+        query_group1 = {
+            "$group": {
+                "_id": "$candidate_info",
+                "count": {"$sum": 1}
+            }
+        }
+        query_add_fields1 = {
+            "$addFields": {
+                "politicalparty": "$_id.politicalparty"
+            }
+        }
+        query_lookup2 = {
+            "$lookup": {
+                "from": "politicalparty",
+                "localField": "politicalparty.$id",
+                "foreignField": "_id",
+                "as": "politicalparty_info"
+            }
+        }
+        query_unwind2 = {
+            "$unwind": "$politicalparty_info"
+        }
+        query_group2 = {
+            "$group": {
+                "_id": "$politicalparty_info",
+                "count": {"$sum": "$count"}
+            }
+        }
+        query_add_fields2 = {
+            "$addFields": {
+                "nombre": "$_id.nombre",
+                "id": "$_id._id"
+            }
+        }
+        pipeline = [query_lookup1, query_unwind1, query_group1, query_add_fields1,query_lookup2, query_unwind2,
+                    query_group2, query_add_fields2]
+        return self.query_aggregation(pipeline)
+
 
