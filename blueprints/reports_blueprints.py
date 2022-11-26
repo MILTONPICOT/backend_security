4911021fd461fcf4122894fccbeb366e875134a8
Mzrbx <111446439+Mzrbx@users.noreply.github.com>
2022-11-25 19:31:41 -0500

    Update reports_blueprints.py

--- blueprints/reports_blueprints.py
+++ blueprints/reports_blueprints.py
@@ -1,20 +1,31 @@
 from flask import Blueprint
 from controllers.reports_controller import ReportController
 
-
-report_blueprints = Blueprint('report_blueprints', __name__)
+reports_blueprints = Blueprint('report_controller', __name__)
 report_controller = ReportController()
 
 
-@report_blueprints.route("/reports/highest_votes_candidate", methods=['GET'])
-def get_highest_votes_candidate_by_table():
-    response = report_controller.get_greatest_votes_candidate_by_table()
+@reports_blueprints.route("/reports/highest_votes", methods=['GET'])
+def get_highest_votes():
+    response = report_controller.get_greatest_votes()
     return response, 200
 
 
-@report_blueprints.route("/reports/votes", methods=['GET'])
-def get_total_votes_candidates():
-    response = report_controller.get_total_votes_candidates()
+@reports_blueprints.route("/reports/result_by_table", methods=['GET'])
+def get_result_by_table():
+    response = report_controller.get_result_by_table()
+    return response, 200
+
+
+@reports_blueprints.route("/reports/result_by_candidate", methods=['GET'])
+def get_result_by_candidate():
+    response = report_controller.get_result_by_candidate()
+    return response, 200
+
+
+@reports_blueprints.route("/reports/result_by_politicalparty", methods=['GET'])
+def get_result_by_politicalparty():
+    response = report_controller.get_result_by_politicalparty()
     return response, 200
 
 
